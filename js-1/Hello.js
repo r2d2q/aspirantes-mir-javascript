52554014ded2d203e6aea1023222788a34385daf
carlos <r2d2q76@gmail.com>
2023-05-10 23:44:24 -0500

    modificacion hola

---
+++ js-1/Hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola (nombre){
+console.log("hola "+nombre+"!")
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
